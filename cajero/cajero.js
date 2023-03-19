7238669755949d947c56fa47c90ceb5bd75098fd
Maryann93 <angelica.perez.polanias@outlook.com>
2023-03-19 12:50:37 -0500

    userPasswor

---
+++ cajero/cajero.js
@@ -0,0 +1,30 @@
+
+let usuario = "Emma", clave = "9511";
+
+usuario = parseInt (prompt("Usuario"));
+const User = "Emma";
+clave = parseInt(prompt("Clave"));
+const Pin = 9511;
+
+if (usuario == "Emma", clave == Pin){
+    alert("El saldo en la cuenta es $1000");
+} if (usuario != User, clave != Pin) {
+    usuario = parseInt(prompt("Ingrese Usuario"));
+    clave = parseInt(prompt("Ingrese Clave"));
+    alert("usuario o clave incorrecto, tiene 2 intentos");
+    
+} if (usuario != User, clave != Pin) {
+    usuario = parseInt(prompt("Ingrese Usuario"));
+    clave = parseInt(prompt("Ingrese Clave"));
+    alert("usuario o clave incorrecto, tiene 1 intentos");
+        
+} if (usuario != User, clave != Pin) {
+    usuario = parseInt(prompt("Ingrese Usuario"));   
+    clave = parseInt(prompt("Ingrese Clave"));
+    alert("usuario o clave incorrecto, ultimo intento");
+   
+}  if (usuario != User, clave != Pin) {
+    usuario = parseInt(prompt("Ingrese Usuario"));   
+    clave = parseInt(prompt("Ingrese Clave"));
+    alert("cuenta bloqueada");
+}
